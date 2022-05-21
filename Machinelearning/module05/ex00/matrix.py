e341f5daf7deb164629d80a3d06b4d5240ce1d01
Kiriam <kiriamblu@gmail.com>
2022-05-21 12:27:56 +0200

    Div done

--- Machinelearning/module05/ex00/matrix.py
+++ Machinelearning/module05/ex00/matrix.py
@@ -61,6 +61,20 @@ class Matrix():
 		except:
 			raise ArithmeticError("Something happend while adding")
 
+	def __radd__(self, mat2):
+		if not isinstance(mat2, Matrix):
+			raise OperatorError("Second arg is not Mat")
+		if self.shape != mat2.shape:
+			raise OperatorError("Different sizes")
+		try:
+			tmp = Matrix(self.shape)
+			for x in range(self.shape[0]):
+				for y in range(self. shape[1]):
+					tmp.mat[x][y] = self.mat[x][y] + mat2.mat[x][y]
+			return tmp	
+		except:
+			raise ArithmeticError("Something happend while adding")
+
 	def __sub__(self, mat2):
 		if not isinstance(mat2, Matrix):
 			raise OperatorError("Second arg is not Mat")
@@ -75,6 +89,68 @@ class Matrix():
 		except:
 			raise ArithmeticError("Something happend while adding")
 
+	def __rsub__(self, mat2):
+		if not isinstance(mat2, Matrix):
+			raise OperatorError("Second arg is not Mat")
+		if self.shape != mat2.shape:
+			raise OperatorError("Different sizes")
+		try:
+			tmp = Matrix(self.shape)
+			for x in range(self.shape[0]):
+				for y in range(self. shape[1]):
+					tmp.mat[x][y] = self.mat[x][y] - mat2.mat[x][y]
+			return tmp	
+		except:
+			raise ArithmeticError("Something happend while adding")
+
+	def __mul__(self, mat2):
+		if not isinstance(mat2, (Matrix, int, float)):
+			raise OperatorError("Second arg isnt good")
+		try:
+			if isinstance(mat2, (int, float)):
+				tmp = Matrix(self.shape)
+				for x in range(self.shape[0]):
+					for y in range(self. shape[1]):
+						tmp.mat[x][y] = self.mat[x][y] * mat2
+			elif isinstance(mat2, Matrix):
+				if self.shape[0] == mat2.shape[1]:
+					tmp = Matrix((self.shape[0], mat2.shape[1]))
+					for x in range(self.shape[0]):
+						for y in range(mat2.shape[0]):
+							for z in range(self.shape[1]):
+								tmp.mat[x][y] += self.mat[x][z] * mat2.mat[z][y]
+				else:
+					raise ArithmeticError("Not posible multiplication")
+			return tmp
+		except:
+			raise ArithmeticError("Something happend while multiplicating")
+
+	def __truediv__(self, mat2):
+		if isinstance(mat2, (Matrix)):
+			raise OperatorError("There are non right ways to do this")
+			try:
+				if(mat2.shape == (1, 1)):
+					tmp = Matrix(self.shape)
+					for x in range(self.shape[0]):
+						for y in range(self.shape[1]):
+							tmp.mat[x][y] = self.mat[x][y] / mat2.mat[0][0]
+					return tmp
+			except:
+				raise ArithmeticError("Something happend while trying to div")
+		if isinstance(mat2, (int, float)) and mat2 == 0:
+			raise ZeroDivisionError("This is a bad idea to divide by 0.")
+		elif isinstance(mat2, (int, float)):
+			try:
+				tmp = Matrix(self.shape)
+				for x in range(self.shape[0]):
+					for y in range(self.shape[1]):
+						tmp.mat[x][y] = self.mat[x][y] / mat2
+				return tmp
+			except:
+				raise ArithmeticError("Something happend while trying to div")
+
+
+
 
 ######################################Tests##############################################################
 
@@ -82,7 +158,7 @@ class Matrix():
 mat = Matrix([[1, 1, 1],[0, 1, 0],[0, 1, 0]])
 mat2 = Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
-mat3 = mat - mat2
+mat3 = mat / 2
 print(mat3.mat)
 
 
